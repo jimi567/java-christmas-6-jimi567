9e7c2807b5270d71aa5bba640f94371858281618
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-12 14:05:45 +0900

    feat(EventBadge) : 이벤트 배지 Enum추가

---
+++ src/main/java/christmas/contant/EventBadge.java
@@ -0,0 +1,23 @@
+package christmas.contant;
+
+public enum EventBadge {
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000);
+
+    private String name;
+    private int minimumBenefitAmount;
+
+    EventBadge(String name, int minimumBenefitAmount) {
+        this.name = name;
+        this.minimumBenefitAmount = minimumBenefitAmount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinimumBenefitAmount() {
+        return minimumBenefitAmount;
+    }
+}
