daca9a9528262db2f76ee6099a7006eeabea6ee3
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-13 12:35:08 +0900

    feat(Error) : 에러문 추가
    유효하지않은 날짜 입력에 대한 메세지,
    유효하지않느 주문 입력에 대한 메세지 추가

--- src/main/java/christmas/contant/Error.java
+++ src/main/java/christmas/contant/Error.java
@@ -1,8 +1,10 @@
 package christmas.contant;
 
 public enum Error {
-    ERROR("[ERROR]"),
-    RE_TRY_PROMPT(" 다시 입력해 주세요.");
+    ERROR("[ERROR] "),
+    RE_TRY_PROMPT(" 다시 입력해 주세요."),
+    NOT_VALIDATE_DAY_ERROR(ERROR.get() + "유효하지 않은 날짜입니다." + RE_TRY_PROMPT.get()),
+    NOT_VALIDATE_ORDER_ERROR(ERROR.get() + "유효하지 않은 주문입니다." + RE_TRY_PROMPT.get());
 
     private final String message;
 
@@ -14,7 +16,7 @@ public enum Error {
         return message;
     }
 
-    public void throwException() {
+    public void throwError() {
         throw new IllegalArgumentException(get());
     }
 }
