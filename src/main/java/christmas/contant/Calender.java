7214dbc472a8b221066aa437d920006f1a3a37eb
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-13 12:34:08 +0900

    fix(Calender) : 각 상수 리스트를 불변객체로 변경
    collectors.toList() - > toList()로 변경해
    추후 add,remove같은 함수를 지원하지 않도록 변경

--- src/main/java/christmas/contant/Calender.java
+++ src/main/java/christmas/contant/Calender.java
@@ -1,17 +1,16 @@
 package christmas.contant;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public enum Calender {
     EVENT_DAYS(IntStream.rangeClosed(1, 31)
             .boxed()
-            .collect(Collectors.toList())),
+            .toList()),
     D_DAY_EVENT_DAYS(IntStream.rangeClosed(1, 25)
             .boxed()
-            .collect(Collectors.toList())),
-    Star_DAYS(List.of(3, 10, 17, 24, 31, 25)),
+            .toList()),
+    STAR_DAYS(List.of(3, 10, 17, 24, 31, 25)),
     WEEKEND_DAYS(List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30));
 
     private final List<Integer> days;
