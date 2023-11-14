56748ab88ff7f482bc8a93b6f41debe0ae534f09
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-14 15:10:58 +0900

    test(GiftDiscountPolicyTest) : 증정 혜택 적용 가능한지와 혜택 금액을 얻는 기능 테스트 작성

---
+++ src/test/java/christmas/domain/discount/GiftDiscountPolicyTest.java
@@ -0,0 +1,48 @@
+package christmas.domain.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.contant.MenuBoard;
+import christmas.domain.customer.Customer;
+import christmas.domain.customer.DecemberVisitDay;
+import christmas.domain.customer.OrderMenu;
+import christmas.domain.menu.Menu;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GiftDiscountPolicyTest {
+
+    DiscountPolicy discountPolicy = new GiftDiscountPolicy();
+    OrderMenu orderMenu;
+
+    @BeforeEach
+    void setOrderMenu() {
+        HashMap<Menu, Integer> orderMenuSource = new HashMap<>();
+        //양송이 수프 20개 주문하면 12만원
+        orderMenuSource.put(new Menu("양송이수프"), 20);
+        orderMenu = new OrderMenu(orderMenuSource);
+    }
+
+    @Test
+    @DisplayName("고객의 총 주문 금액이 12만원 이상이면 혜택적용이 가능하다.")
+    void testApplicableEventCaseNotApplicable() {
+        Customer customer = new Customer(new DecemberVisitDay(31), orderMenu);
+
+        assertThat(discountPolicy.applicableEvent(customer)).isTrue();
+    }
+
+    @Test
+    @DisplayName("증정 혜택의 혜택 금액을 리턴한다.")
+    void testGetDiscountAmount() {
+        Customer customer = new Customer(new DecemberVisitDay(1), orderMenu);
+
+        int result = discountPolicy.getDiscountAmount(customer);
+        int expected = MenuBoard.CHAMPAGNE.getPrice();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+}
+
