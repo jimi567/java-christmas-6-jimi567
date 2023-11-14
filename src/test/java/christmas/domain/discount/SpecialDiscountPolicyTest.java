428640de0d0398850b5f8f733b559a95316b0b58
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-14 15:00:28 +0900

    test(SpecialDiscountPolicyTest) : 특별 혜택 적용 가능한 지와 혜택 금액을 얻는 기능 테스트 작성

---
+++ src/test/java/christmas/domain/discount/SpecialDiscountPolicyTest.java
@@ -0,0 +1,60 @@
+package christmas.domain.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.customer.Customer;
+import christmas.domain.customer.DecemberVisitDay;
+import christmas.domain.customer.OrderMenu;
+import christmas.domain.menu.Menu;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SpecialDiscountPolicyTest {
+    DiscountPolicy discountPolicy = new SpecialDiscountPolicy();
+    OrderMenu orderMenu;
+
+    @BeforeEach
+    void setOrderMenu() {
+        HashMap<Menu, Integer> orderMenuSource = new HashMap<>();
+        orderMenuSource.put(new Menu("티본스테이크"), 1);
+        orderMenuSource.put(new Menu("바비큐립"), 1);
+        orderMenuSource.put(new Menu("초코케이크"), 2);
+        orderMenuSource.put(new Menu("제로콜라"), 1);
+        orderMenu = new OrderMenu(orderMenuSource);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 6, 11, 12, 30})
+    @DisplayName("고객의 방문날짜에 별이 없으면 혜택적용이 불가능하다.")
+    void testApplicableEventCaseApplicable(int date) {
+        Customer customer = new Customer(new DecemberVisitDay(date), orderMenu);
+
+        assertThat(discountPolicy.applicableEvent(customer)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 17, 24, 31, 25})
+    @DisplayName("고객의 방문날짜가 별이 있으면 혜택적용이 가능하다.")
+    void testApplicableEventCaseNotApplicable(int date) {
+        Customer customer = new Customer(new DecemberVisitDay(date), orderMenu);
+
+        assertThat(discountPolicy.applicableEvent(customer)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 17, 24, 31, 25})
+    @DisplayName("특별 혜택의 혜택 금액을 리턴한다.")
+    void testGetDiscountAmount(int date) {
+        Customer customer = new Customer(new DecemberVisitDay(date), orderMenu);
+
+        int result = discountPolicy.getDiscountAmount(customer);
+        int expected = 1000;
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+}
+
